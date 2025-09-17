0e1120df93a61bd7a593d42fd5144ff8813ac3cd
Dzmitry Pirozhnik <30670539+dipiro@users.noreply.github.com>
2025-09-17 09:41:12 -0400

    Update Package.swift
    commit

--- Package.swift
+++ Package.swift
@@ -4,25 +4,23 @@ import PackageDescription
 let package = Package(
     name: "PostHog",
     platforms: [
-        // TODO: add  .visionOS(.v1), when SPM is >= 5.9
+        // TODO: add .visionOS(.v1) when SPM is >= 5.9
         .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Делаем библиотеку динамической
         .library(
             name: "PostHog",
+            type: .dynamic,
             targets: ["PostHog"]
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Quick/Quick.git", from: "6.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "12.0.0"),
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "9.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PostHog",
             dependencies: ["phlibwebp"],
@@ -30,6 +28,12 @@ let package = Package(
             resources: [
                 .copy("Resources/PrivacyInfo.xcprivacy"),
             ]
+        ),
+        .target(
+            name: "phlibwebp",
+            path: "vendor/libwebp",
+            publicHeadersPath: ".",
+            cSettings: [.headerSearchPath(".")]
         ),
         .testTarget(
             name: "PostHogTests",
